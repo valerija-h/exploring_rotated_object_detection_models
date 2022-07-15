0f9b5a043dae4c97060fa8985f891f3ed901c876
valerija-h <vholomjova@gmail.com>
2022-07-15 16:31:49 +0100

    Finished commenting baseline model transformations

--- utils/transforms.py
+++ utils/transforms.py
@@ -8,6 +8,10 @@ from PIL.Image import FLIP_LEFT_RIGHT, FLIP_TOP_BOTTOM, AFFINE
 import random
 import cv2
 
+#############################################################################
+# --------------------------- UTILITY FUNCTIONS --------------------------- #
+#############################################################################
+
 
 def re_assign_class(theta, class_mapping):
     for key, value in class_mapping.items():
@@ -19,6 +23,36 @@ def re_assign_class(theta, class_mapping):
 def invert_angle(theta_class, class_mapping):
     theta = (class_mapping[theta_class][0] + class_mapping[theta_class][1]) / 2
     return -theta
+
+def visualise_transforms(data_loader, class_mapping):
+    images, targets = next(iter(data_loader))
+
+    for i in range(len(images)):
+        # convert the image to
+        fig, ax = plt.subplots()
+        image = torchvision.transforms.ToPILImage()(images[i])
+        ax.imshow(image)
+        for b, (xmin, ymin, xmax, ymax) in enumerate(targets[i]['boxes']):
+            w, h = xmax - xmin, ymax - ymin
+            x, y = xmax - (w / 2), ymax - (h / 2)
+            t_range = class_mapping[targets[i]['labels'][b].item()]  # range of theta values [min_t, max_t]
+            t = (t_range[0] + t_range[1]) / 2
+            w_cos, w_sin, h_sin, h_cos = (w / 2) * np.cos(t), (w / 2) * np.sin(t), (h / 2) * np.sin(t), (
+                    h / 2) * np.cos(t)
+            bl_x, bl_y, tl_x, tl_y = x - w_cos + h_sin, y - w_sin - h_cos, x - w_cos - h_sin, y - w_sin + h_cos
+            br_x, br_y, tr_x, tr_y = x + w_cos + h_sin, y + w_sin - h_cos, x + w_cos - h_sin, y + w_sin + h_cos
+            plt.plot([bl_x, tl_x], [bl_y, tl_y], c='black')
+            plt.plot([br_x, tr_x], [br_y, tr_y], c='black')
+            rect = patches.Rectangle((x - (w / 2), y - (h / 2)), w, h, linewidth=1, edgecolor='r', facecolor='none',
+                                     transform=Affine2D().rotate_around(*(x, y), t) + ax.transData)
+            ax.add_patch(rect)
+
+        plt.show()
+
+
+def collate_fn(batch):
+    """ Needed for data loader to load images with batch size > 1 """
+    return tuple(zip(*batch))
 
 
 #############################################################################
@@ -153,11 +187,10 @@ class RandomShift(object):
 
 
 class RandomHorizontalFlip(object):
-    """ Randomly horizontally flips the Image with the probability *p*
-    Args:
-    p (float): The probability with which the image is flipped
+    """ Randomly applies a horizontal flip on the image with a specified probability (p).
+      :param class_mapping: (dict) a dictionary mapping classes to theta/rotation values (see Dataset objects).
+      :param p: (float) the probability with which the image is flipped
     """
-
     def __init__(self, class_mapping, p=0.5):
         self.class_mapping = class_mapping
         self.p = p
@@ -167,24 +200,23 @@ class RandomHorizontalFlip(object):
         img_center = np.array((img.size[1], img.size[0]))[::-1] / 2
         img_center = np.hstack((img_center, img_center))
         if random.random() < self.p:
-            img = img.transpose(FLIP_LEFT_RIGHT)
+            img = img.transpose(FLIP_LEFT_RIGHT)  # flip image
+            # transform grasping box co-ordinates
             target['boxes'][:, [0, 2]] += 2 * (img_center[[0, 2]] - target['boxes'][:, [0, 2]])
             box_w = abs(target['boxes'][:, 0] - target['boxes'][:, 2])
-
             target['boxes'][:, 0] -= box_w
             target['boxes'][:, 2] += box_w
-
+            # adjust grasping box rotation classes
             for i, l in enumerate(target['labels']):
                 target['labels'][i] = re_assign_class(invert_angle(l, self.class_mapping), self.class_mapping)
         return img, target
 
 
 class RandomVerticalFlip(object):
-    """ Randomly vertically flips the Image with the probability *p*
-    Args:
-    p (float): The probability with which the image is flipped
+    """ Randomly applies a vertical flip on the image with a specified probability (p).
+      :param class_mapping: (dict) a dictionary mapping classes to theta/rotation values (see Dataset objects).
+      :param p: (float) the probability with which the image is flipped
     """
-
     def __init__(self, class_mapping, p=0.5):
         self.class_mapping = class_mapping
         self.p = p
@@ -194,25 +226,22 @@ class RandomVerticalFlip(object):
         img_center = np.array((img.size[1], img.size[0]))[::-1] / 2
         img_center = np.hstack((img_center, img_center))
         if random.random() < self.p:
-            img = img.transpose(FLIP_TOP_BOTTOM)
+            img = img.transpose(FLIP_TOP_BOTTOM)  # flip image
+            # transform grasping box co-ordinates
             target['boxes'][:, [1, 3]] += 2 * (img_center[[1, 3]] - target['boxes'][:, [1, 3]])
-
             box_h = abs(target['boxes'][:, 1] - target['boxes'][:, 3])
-
             target['boxes'][:, 1] -= box_h
             target['boxes'][:, 3] += box_h
+            # adjust grasping box rotation classes
             for i, l in enumerate(target['labels']):
                 target['labels'][i] = re_assign_class(invert_angle(l, self.class_mapping), self.class_mapping)
         return img, target
 
 
 class CentreCrop(object):
-    """Crop randomly the image in a sample.
-    Args:
-        output_size (tuple or int): Desired output size. If int, square crop
-            is made.
+    """ Crop the image by a specified amount from the centre.
+      :param output_size: (tuple or int) desired output size. If given an int - a square crop is made.
     """
-
     def __init__(self, output_size):
         assert isinstance(output_size, (int, tuple))
         if isinstance(output_size, int):
@@ -223,37 +252,29 @@ class CentreCrop(object):
 
     def __call__(self, data):
         img, target = data[0], data[1]
-        width, height = img.size  # get dimensions
+        width, height = img.size  # get image dimensions
+        # adjust the grasping box co-ordinates
         left = (width - self.output_size[0]) / 2
         top = (height - self.output_size[1]) / 2
         right = (width + self.output_size[0]) / 2
         bottom = (height + self.output_size[1]) / 2
-
-        # scale_width = self.output_size[0]/width
-        # scale_height = self.output_size[1]/height
-        # target['boxes'][:, 0] *= scale_width  # change xmin
-        # target['boxes'][:, 2] *= scale_width  # change xmax
-        # target['boxes'][:, 1] *= scale_height  # change ymin
-        # target['boxes'][:, 3] *= scale_height  # change yax
         target['boxes'][:, 0] -= left  # change xmin
         target['boxes'][:, 2] -= left  # change xmax
         target['boxes'][:, 1] -= top  # change ymin
-        target['boxes'][:, 3] -= top  # change yax
-
+        target['boxes'][:, 3] -= top  # change ymax
         # recalculate new area
         target['area'] = (target['boxes'][:, 3] - target['boxes'][:, 1]) * (
                     target['boxes'][:, 2] - target['boxes'][:, 0])
         # crop the center of the image
         img = img.crop((left, top, right, bottom))
-
         return img, target
 
 
 class CustomCrop(object):
-    """Crop randomly the image in a sample.
-    Args:
-        output_size (tuple or int): Desired output size. If int, square crop
-            is made.
+    """ Crop the image by a specified amount of pixels from the edges.
+     :param output_size: (tuple or int) desired output size. If given an int - a square crop is made.
+     :param top: (float) number of pixels to crop from the top.
+     :param left: (float) number of pixels to crop from the left.
     """
 
     def __init__(self, top, left, output_size):
@@ -268,25 +289,21 @@ class CustomCrop(object):
 
     def __call__(self, data):
         img, target = data[0], data[1]
+        # calculate the remaining amount to crop from the right and bottom sides of images
         right = self.left + self.output_size[0]
         bottom = self.top + self.output_size[1]
-
+        # adjust the grasping box co-ordinates
         target['boxes'][:, 0] -= self.left  # change xmin
         target['boxes'][:, 2] -= self.left  # change xmax
         target['boxes'][:, 1] -= self.top  # change ymin
-        target['boxes'][:, 3] -= self.top  # change yax
-
-        # recalculate new area - I don't think you need to recalc it?
-        # target['area'] = (target['boxes'][:, 3] - target['boxes'][:, 1]) * (target['boxes'][:, 2] - target['boxes'][:, 0])
-        # crop the center of the image
+        target['boxes'][:, 3] -= self.top  # change ymax
+        # crop the image
         img = img.crop((self.left, self.top, right, bottom))
-
         return img, target
 
 
 class ToTensor(object):
-    """ Convert image and bounding boxes to Tensors. """
-
+    """ Convert the image and bounding boxes to Tensors. """
     def __call__(self, data):
         img, target = data[0], data[1]
         new_img = torchvision.transforms.ToTensor()(img).float()
@@ -294,33 +311,3 @@ class ToTensor(object):
         target["labels"] = torch.as_tensor(target["labels"], dtype=torch.int64)
         return new_img, target
 
-
-def visualise_transforms(data_loader, class_mapping):
-    images, targets = next(iter(data_loader))
-
-    for i in range(len(images)):
-        # convert the image to
-        fig, ax = plt.subplots()
-        image = torchvision.transforms.ToPILImage()(images[i])
-        ax.imshow(image)
-        for b, (xmin, ymin, xmax, ymax) in enumerate(targets[i]['boxes']):
-            w, h = xmax - xmin, ymax - ymin
-            x, y = xmax - (w / 2), ymax - (h / 2)
-            t_range = class_mapping[targets[i]['labels'][b].item()]  # range of theta values [min_t, max_t]
-            t = (t_range[0] + t_range[1]) / 2
-            w_cos, w_sin, h_sin, h_cos = (w / 2) * np.cos(t), (w / 2) * np.sin(t), (h / 2) * np.sin(t), (
-                    h / 2) * np.cos(t)
-            bl_x, bl_y, tl_x, tl_y = x - w_cos + h_sin, y - w_sin - h_cos, x - w_cos - h_sin, y - w_sin + h_cos
-            br_x, br_y, tr_x, tr_y = x + w_cos + h_sin, y + w_sin - h_cos, x + w_cos - h_sin, y + w_sin + h_cos
-            plt.plot([bl_x, tl_x], [bl_y, tl_y], c='black')
-            plt.plot([br_x, tr_x], [br_y, tr_y], c='black')
-            rect = patches.Rectangle((x - (w / 2), y - (h / 2)), w, h, linewidth=1, edgecolor='r', facecolor='none',
-                                     transform=Affine2D().rotate_around(*(x, y), t) + ax.transData)
-            ax.add_patch(rect)
-
-        plt.show()
-
-
-def collate_fn(batch):
-    """ Needed for data loader to load images with batch size > 1 """
-    return tuple(zip(*batch))
